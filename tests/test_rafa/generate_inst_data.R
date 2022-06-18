b345774912cf13173fe4ce5243cf88d951a88641
rafapereirabr <rafael.pereira.br@gmail.com>
2022-06-18 00:28:54 -0300

    add reprex bho 30 min transit 2019 aopdata

--- tests/test_rafa/generate_inst_data.R
+++ tests/test_rafa/generate_inst_data.R
@@ -37,3 +37,103 @@ head(ttm)
 nrow(ttm)
 
 fwrite(ttm, file = 'ttm_poa.csv' )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+library(data.table)
+library(sf)
+library(ggplot2)
+library(mapview)
+mapview::mapviewOptions(platform = 'mapdeck')
+
+sigla <- 'bho'
+
+
+########## select radius  --------------------------------------------------
+# acces <- aopdata::read_access(city=sigla, year=2019, geometry = T)
+# acces <- readRDS('L:/Proj_acess_oport/data/acesso_oport/output_base_final/2019/dados2019_AcessOport_access_tpcar_v1.0.rds')
+acces_for <- subset(acces, sigla_muni ==sigla & pico==1 & modo =='tp')
+
+center <- acces_for$id_hex[which.max(acces_for$CMATT30)]
+
+buff <- subset(acces_for, id_hex == center) |>
+          st_centroid()  |>
+          st_buffer(dist = 5000)
+
+buff$CMATT30 <- NULL
+
+ggplot() +
+  geom_sf(data=acces_for, aes(fill=CMATT30), color=NA) +
+  geom_sf(data=buff, color='red', fill='red', alpha=.5)
+
+inter <- st_intersection(acces_for, buff )
+
+ggplot() +
+  geom_sf(data=inter, aes(fill=CMATT30), color=NA)
+
+
+
+########## travel time matrix --------------------------------------------------
+# read ttm
+ttm_files <- list.files('E:/data/ttmatrix_fixed/2019', full.names = T)
+ttm_for <- ttm_files[ ttm_files %like% sigla]
+
+
+dt <- readRDS(ttm_for)
+head(dt)
+
+ttm <- subset(dt,
+              mode =='transit' &
+              pico == 1 &
+              ano == 2019 &
+              city  == sigla &
+              travel_time <= 30)
+
+
+ttm[, c('pico', 'city', 'ano', 'mode') := NULL]
+names(ttm) <- c('from_id', 'to_id', 'travel_time')
+head(ttm)
+
+
+########## land use  --------------------------------------------------
+land_for <- aopdata::read_landuse(city=sigla, year=2019, geometry = F)
+land_for <- land_for[, .(id_hex, P001, T001, E001)]
+names(land_for) <- c('id', 'population', 'jobs', 'schools')
+
+
+# merge opportunities data
+ttm[land_for, on=c('from_id'='id'), population   := i.population  ]
+ttm[land_for, on=c('to_id'='id'), jobs  := i.jobs ]
+ttm[land_for, on=c('to_id'='id'), schools  := i.schools ]
+
+
+ttm2 <- subset(ttm, population > 0 | jobs >0 | schools >0 )
+ttm2 <- subset(ttm2, from_id %in% unique(inter$id_hex) )
+ttm2 <- subset(ttm2, to_id %in% unique(inter$id_hex) )
+head(ttm2)
+
+saveRDS(ttm, './inst/extdata/ttm_bho.rds', compress = TRUE)
+
+
+########## grid  --------------------------------------------------
+grid <- aopdata::read_grid(city = sigla)
+
+grid$abbrev_muni <- NULL
+grid$name_muni  <- NULL
+grid$code_muni <- NULL
+names(grid)[1] <- 'id'
+
+grid2 <- subset(grid, id %in% unique(inter$id_hex) )
+
+saveRDS(grid, './inst/extdata/grid_bho.rds', compress = TRUE)
