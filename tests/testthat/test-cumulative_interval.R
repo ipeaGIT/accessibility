d086de936e68ef92884c0220bc9bf30add72e2c4
rafapereirabr <rafa.pereira.br@gmail.com>
2022-06-19 18:46:21 -0300

    replace parameter of cumulative_time_interval

--- tests/testthat/test-cumulative_interval.R
+++ tests/testthat/test-cumulative_interval.R
@@ -7,15 +7,13 @@ testthat::skip_on_cran()
 
 default_tester <- function(data = ttm,
                            opportunity_col = 'schools',
-                           start = 20,
-                           end = 25,
+                           interval = c(20, 25),
                            stat = 'mean',
                            by_col='from_id') {
 
   results <- accessibility::cumulative_time_interval(data = data,
                                       opportunity_col = opportunity_col,
-                                      start = start,
-                                      end = end,
+                                      interval = interval,
                                       stat = stat,
                                       by_col = by_col)
   return(results)
@@ -41,13 +39,14 @@ test_that("adequately raises errors", {
   expect_error(default_tester(stat = 999))
 
 
-  # start / end values are not positive numeric
-  expect_error(default_tester(start = "banana"))
-  expect_error(default_tester(end = "banana"))
-  expect_error(default_tester(start = -3))
-  expect_error(default_tester(end = -3))
-  expect_error(is(default_tester(start = Inf), "data.table"))
-  expect_error(is(default_tester(end = Inf), "data.table"))
+  # interval values are not positive numeric
+  expect_error(default_tester(interval = c(1,2,3)))
+  expect_error(default_tester(interval = c(5,5)))
+  expect_error(default_tester(interval = c(1,-2)))
+  expect_error(default_tester(interval = c(1,Inf)))
+  expect_error(default_tester(interval = "banana"))
+  expect_error(default_tester(interval = -3))
+  expect_error(is(default_tester(interval = Inf), "data.table"))
 
 })
 
@@ -63,15 +62,15 @@ test_that("output is correct", {
 
 
   # different opportunity_col
-  expect_is( default_tester(opportunity_col = 'population'), "data.table")
+  expect_is( default_tester(opportunity_col = 'jobs'), "data.table")
 
   # different by_col
   expect_is( default_tester(by_col = 'from_id'), "data.table")
 
   # different interval values
-  expect_is( default_tester(start = 1), "data.table")
-  expect_is( default_tester(end = 60), "data.table")
-  expect_is( default_tester(start=30, end = 20), "data.table")
+  expect_is( default_tester(interval = c(10, 15)), "data.table")
+  expect_is( default_tester(interval = c(0, 15)), "data.table")
+  expect_is( default_tester(interval = c(20, 10)), "data.table")
 
   # different summary stat
   expect_is( default_tester(stat = 'median'), "data.table")
