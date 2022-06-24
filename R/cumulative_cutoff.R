06677c9616faad8a2f901e6676cabccc7725e381
rafapereirabr <rafa.pereira.br@gmail.com>
2022-06-24 13:41:44 -0300

    add travel_cost_col to cumulative_cutoff

--- R/cumulative_cutoff.R
+++ R/cumulative_cutoff.R
@@ -5,9 +5,10 @@
 #' travel time cutoff specified by the user.
 #'
 #' @template input_data
-#' @template opportunity_col
-#' @template by_col
 #' @param cutoff A `numeric` value indicating the maximum travel time considered.
+#' @template opportunity_col
+#' @template travel_cost_col
+#' @template by_col
 #'
 #' @return A `data.table` object.
 #' @family Cumulative access
@@ -20,36 +21,41 @@
 #'
 #'# Active accessibility: number of schools accessible from each origin
 #'df <- cumulative_time_cutoff(data = ttm,
-#'                             opportunity_col = 'schools',
 #'                             cutoff = 30,
+#'                             opportunity_col = 'schools',
+#'                             travel_cost_col='travel_time',
 #'                             by_col = 'from_id')
 #'head(df)
 #'
 #'# Passive accessibility: number of people that can reach each destination
 #'df <- cumulative_time_cutoff(data = ttm,
-#'                             opportunity_col = 'population',
 #'                             cutoff = 30,
+#'                             opportunity_col = 'population',
+#'                             travel_cost_col='travel_time',
 #'                             by_col = 'to_id')
 #'head(df)
 #' @export
-cumulative_time_cutoff <- function(data, opportunity_col, cutoff, by_col){
+cumulative_time_cutoff <- function(data, cutoff, opportunity_col, travel_cost_col='travel_time', by_col){
 
   # check inputs ------------------------------------------------------------
-  checkmate::test_data_frame(data)
-  checkmate::test_string(opportunity_col)
-  checkmate::test_string(by_col)
+  checkmate::assert_data_frame(data)
+  checkmate::assert_string(opportunity_col)
+  checkmate::assert_string(travel_cost_col)
+  checkmate::assert_string(by_col)
   checkmate::assert_number(cutoff, lower = 0, finite = TRUE)
 
   checkmate::assert_names(names(data), must.include = opportunity_col,
                           .var.name = "data")
   checkmate::assert_names(names(data), must.include = by_col,
                           .var.name = "data")
+  checkmate::assert_names(names(data), must.include = travel_cost_col,
+                          .var.name = "data")
 
 
   # calculate access -----------------------------------------------------------
   data.table::setDT(data)
 
-  access <- data[travel_time <= cutoff, .(access = sum(get(opportunity_col)) ), by=c(by_col)]
+  access <- data[get(travel_cost_col) <= cutoff, .(access = sum(get(opportunity_col)) ), by=c(by_col)]
 
   return(access)
 }
--- tests/testthat/test-cumulative_cutoff.R
+++ tests/testthat/test-cumulative_cutoff.R
@@ -6,13 +6,15 @@ context("Cumulative cutoff function")
 testthat::skip_on_cran()
 
 default_tester <- function(data = ttm,
-                           opportunity_col = 'schools',
                            cutoff = 20,
+                           opportunity_col = 'schools',
+                           travel_cost_col = 'travel_time',
                            by_col='from_id') {
 
   results <- accessibility::cumulative_time_cutoff(data = data,
-                                      opportunity_col = opportunity_col,
                                       cutoff = cutoff,
+                                      opportunity_col = opportunity_col,
+                                      travel_cost_col = travel_cost_col,
                                       by_col = by_col)
   return(results)
   }
@@ -29,8 +31,10 @@ test_that("adequately raises errors", {
   # opportunity_col and by_col do not exist in data input
   expect_error(default_tester(opportunity_col = 'banana'))
   expect_error(default_tester(by_col = 'banana'))
+  expect_error(default_tester(by_col = 'travel_cost_col'))
   expect_error(default_tester(opportunity_col = 999))
   expect_error(default_tester(by_col = 999))
+  expect_error(default_tester(by_col = travel_cost_col))
 
   # cutoff value is not positive numeric
   expect_error(default_tester(cutoff = "banana"))
