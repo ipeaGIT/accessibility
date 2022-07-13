945331f7a1a0515821d1b85c525526f1d72bc76f
Daniel Herszenhut <dhersz@gmail.com>
2022-07-13 16:38:44 -0300

    slight change to active param documentation

--- man/roxygen/templates/active.R
+++ man/roxygen/templates/active.R
@@ -1,4 +1,3 @@
 #' @param active A logical. Whether to calculate active accessibility (the
 #'   of opportunities that can be reached from a given origin, the default) or
-#'   passive accessibility (how many opportunities/people can reach a given
-#'   destination).
+#'   passive accessibility (by how many people each destination can be reached).
