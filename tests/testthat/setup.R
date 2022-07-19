dcd51ba5edad0b6e576d91bdbac6820899b78084
Daniel Herszenhut <dhersz@gmail.com>
2022-07-19 17:59:39 -0300

    removed unused datasets

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -5,4 +5,3 @@ travel_matrix_list <- list(transit = travel_matrix, transit2 = travel_matrix)
 travel_matrix <- data.table::rbindlist(travel_matrix_list, idcol = "mode")
 
 land_use_data <- readRDS(file.path(data_dir, "land_use_data.rds"))
-ttm <- readRDS(file.path(data_dir, "ttm_bho.rds"))
