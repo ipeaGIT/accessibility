294c5e29f5fa768153394be2adabb4e07b23aad3
rafapereirabr <rafa.pereira.br@gmail.com>
2022-07-29 10:22:12 -0300

    minor typos

--- R/cost_to_closest.R
+++ R/cost_to_closest.R
@@ -11,7 +11,7 @@
 #'   that should be considered. Defaults to 1.
 #' @template group_by
 #' @template active
-#' @param fill_missing_ids A `logical`. Calculating minimum trave cost to
+#' @param fill_missing_ids A `logical`. Calculating minimum travel cost to
 #'   closest N number of opportunities may result in missing ids in the output
 #'   if they cannot reach the specified amount of opportunities across all
 #'   destinations they can reach. For example, estimating the minimum travel
