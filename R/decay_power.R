e7372e33f26efcc2832e28135ee3693063287cad
Daniel Herszenhut <dhersz@gmail.com>
2023-03-24 15:26:26 -0300

    new decay_power()

--- R/decay_power.R
+++ R/decay_power.R
@@ -4,8 +4,8 @@
 #' calculating functions.
 #' @template description_generic_cost
 #'
-#' @param decay_value A `numeric`. The calibration parameter to be used as the
-#'   exponent in the inverse power function.
+#' @param decay_value A `numeric` vector. The calibration parameters to be used
+#'   as the exponents in the inverse power function.
 #'
 #' @template return_decay_function
 #'
@@ -14,18 +14,35 @@
 #' @examples
 #' weighting_function <- decay_power(decay_value = 0.1)
 #'
-#' weighting_function(20)
+#' weighting_function(c(20, 35))
 #'
-#' weighting_function(35)
+#' weighting_function <- decay_power(decay_value = c(0.1, 0.2))
+#'
+#' weighting_function(c(20, 35))
 #'
 #' @export
 decay_power <- function(decay_value) {
-  checkmate::assert_number(decay_value, lower = 0, finite = TRUE)
+  checkmate::assert_numeric(
+    decay_value,
+    lower = 0,
+    finite = TRUE,
+    any.missing = FALSE,
+    min.len = 1,
+    unique = TRUE
+  )
 
   weighting_function <- function(travel_cost) {
-    weights <- travel_cost ^ (-decay_value)
-    weights[weights > 1] <- 1
-    return(weights)
+    weights_list <- lapply(
+      decay_value,
+      function(x) {
+        weights <- travel_cost ^ (-x)
+        weights[weights > 1] <- 1
+        weights
+      }
+    )
+    names(weights_list) <- decay_value
+
+    return(weights_list)
   }
 
   return(weighting_function)
--- tests/testthat/test-decay_power.R
+++ tests/testthat/test-decay_power.R
@@ -2,38 +2,60 @@ tester <- function(decay_value = 0.5) decay_power(decay_value)
 
 test_that("adequately raises errors", {
   expect_error(tester("banana"))
-  expect_error(tester(c(0.1, 0.1)))
   expect_error(tester(-0.1))
   expect_error(tester(Inf))
-  expect_error(tester(NULL))
+  expect_error(tester(NA))
+  expect_error(tester(numeric(0)))
+  expect_error(tester(c(0.1, 0.1)))
 })
 
 test_that("output is a decay function that returns a numeric", {
   expect_is(tester(), "function")
 
-  value_test <- tester()
-  expect_equal(value_test(0), 1)
-  expect_equal(value_test(0.5), 1)
-  expect_equal(value_test(1), 1)
-  expect_equal(value_test(16), 0.25)
-  expect_equal(value_test(64), 0.125)
+  output_fn <- tester()
+  output_list <- output_fn(0)
+  expect_is(output_list, "list")
+  expect_length(output_list, 1)
+  expect_named(output_list, "0.5")
 
-  value_test <- tester(0.25)
-  expect_equal(value_test(0), 1)
-  expect_equal(value_test(0), 1)
-  expect_equal(value_test(0.5), 1)
-  expect_equal(value_test(1), 1)
-  expect_equal(value_test(16), 0.5)
-  expect_equal(value_test(256), 0.25)
+  expect_equal(
+    output_fn(c(0, 0.5, 1, 16, 64))[["0.5"]],
+    c(1, 1, 1, 0.25, 0.125)
+  )
+
+  output_fn <- tester(0.25)
+  output_list <- output_fn(0)
+  expect_is(output_list, "list")
+  expect_length(output_list, 1)
+  expect_named(output_list, "0.25")
+
+  expect_equal(
+    output_fn(c(0, 0.5, 1, 16, 256))[["0.25"]],
+    c(1, 1, 1, 0.5, 0.25)
+  )
+
+  output_fn <- tester(c(0.5, 0.25))
+  output_list <- output_fn(0)
+  expect_is(output_list, "list")
+  expect_length(output_list, 2)
+  expect_named(output_list, c("0.5", "0.25"))
+
+  expect_equal(
+    output_fn(c(0, 0.5, 1, 16, 64))[["0.5"]],
+    c(1, 1, 1, 0.25, 0.125)
+  )
+  expect_equal(
+    output_fn(c(0, 0.5, 1, 16, 256))[["0.25"]],
+    c(1, 1, 1, 0.5, 0.25)
+  )
 })
 
-test_that("accepts a numeric vector", {
-  value_test <- tester()
-  expect_equal(value_test(c(0, 0.5, 1, 16, 64)), c(1, 1, 1, 0.25, 0.125))
-})
+test_that("output fn returns empty numeric if receives empty numeric/integer", {
+  output_fn <- tester(c(0.5, 0.25))
 
-test_that("returns empty numeric if receives empty numeric/integer", {
-  value_test <- tester()
-  expect_identical(value_test(integer()), numeric())
-  expect_identical(value_test(numeric()), numeric())
+  expect_identical(output_fn(integer())[["0.5"]], numeric())
+  expect_identical(output_fn(integer())[["0.25"]], numeric())
+
+  expect_identical(output_fn(numeric())[["0.5"]], numeric())
+  expect_identical(output_fn(numeric())[["0.25"]], numeric())
 })
