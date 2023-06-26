4d5d4f252056c71c78b3738a29b2919e108ef9db
Daniel Herszenhut <dhersz@gmail.com>
2023-06-26 10:52:47 -0300

    revert `merge_by_reference()` changes

--- R/merge_by_reference.R
+++ R/merge_by_reference.R
@@ -1,9 +1,8 @@
 #' @keywords internal
 merge_by_reference <- function(data,
-                                land_use_data,
-                                opportunity,
-                                left_df_idcol,
-                                income = NULL) {
+                               land_use_data,
+                               opportunity,
+                               left_df_idcol) {
   # when calculating active accessibility, we want the number of opportunities
   # in the destination. if passive, the number of opportunities in the origin
 
@@ -11,8 +10,6 @@ merge_by_reference <- function(data,
 
   join_vector <- right_df_idcol
   names(join_vector) <- left_df_idcol
-
-  # data <- subset(data, get(left_df_idcol) %in% land_use_data[, get(join_vector)])
 
   data[
     land_use_data,
